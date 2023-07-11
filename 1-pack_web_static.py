59d0536c6f919d623024f16162c6e493d03f0c91
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-07-11 09:23:58 +0000

    Pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A Fabric script that generates a .tgz archive from the contents of
+AirBnB_clone_v2 web_static folder"""
+
+from datetime import datetime
+from fabric.api import local, settings
+import os
+
+
+def do_pack():
+    '''Generates a .tgz archive from the contents of the web_static folder
+    of AirBnB_clone_v2
+
+    Returns:
+        str: Path to the archive created'''
+   
+    if not os.path.isdir("versions"):
+        os.makedirs("versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+
+    print("Packing web_static to {}".format(file_name))
+    with settings(warn_only=True):
+        result = local("tar -cvzf {} web_static".format(file_name))
+        if result.failed:
+            return None
+    file_size = os.stat(file_name).st_size
+    print("web_static packed: {} -> {}Bytes".format(file_name, file_size))
+
+    return file_name
