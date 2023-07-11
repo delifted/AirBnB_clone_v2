d4ff5f26d7b9d83456c9d82ea6a13602d443296f
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-07-11 18:48:07 +0000

    Setup web static with puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,114 +1,87 @@
-# Set up a web server for deployment of the web_static folder
+# Puppet for setup
 
-# 1. Update the apt package index
-exec { 'apt-get update':
-  command => 'apt-get update',
-  path    => '/usr/bin',
-}
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
-# 2. Ensure that nginx is installed
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get update'],
+  ensure   => 'present',
+  provider => 'apt'
 }
 
-# 3. Create the folder /data and its subfolders
-file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+-> file { '/data':
+  ensure  => 'directory'
 }
 
-file { '/data/web_static':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
 }
 
-# 4. Create Create a test HTML file in the test subfolder
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content =>
-  '<html>
-    <head>
-    </head>
-    <body>
-      Holberton School
-    </body>
-  </html>',
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
-# 5. Create a symbolic link to the /data/web_static/releases/test/ folder
-exec { 'ln -sf /data/web_static/releases/test/ /data/web_static/current':
-  path    => '/usr/bin',
-  creates => '/data/web_static/current',
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-# 6. Give ownership of the /data/ folder to the ubuntu user AND group
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path    => '/usr/bin',
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# 7. Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content =>
-  "server {
-          listen 80 default_server;
-          listen [::]:80 default_server;
-
-          root /var/www/html;
-
-          # Add index.php to the list if you are using PHP
-          index index.html index.htm index.nginx-debian.html;
-
-          server_name _;
-
-          location / {
-                  # First attempt to serve request as file, then
-                  # as directory, then fall back to displaying a 404.
-                  try_files \$uri \$uri/ =404;
-          }
-    
-          location /redirect_me {
-                  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-          }
-
-          error_page 404 /custom_404.html;
-          location = /custom_404.html {
-                  root /usr/share/nginx/html;
-                  internal;
-          }
-
-          location /hbnb_static {
-                  alias /data/web_static/current/;
-          }
-  }",
-  notify  => Service['nginx'],
+file { '/var/www':
+  ensure => 'directory'
 }
 
-# 8. Restart nginx
-service { 'nginx':
-  ensure     => 'running',
-  enable     => 'true',
-  hasrestart => 'true',
-  require    => File['/etc/nginx/sites-available/default'],
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+}
+
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
