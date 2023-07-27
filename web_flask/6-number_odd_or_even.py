7cec5d11d3bda8842011d6ef7e05b24125261086
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-07-27 21:14:18 +0100

    Web Flask Framework Project Continued

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -35,9 +35,15 @@ def number(n):
         return f"{n } is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False):
-    return render_template('number_template.html', number=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
 
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    '''even_number = n%2 == 0'''
+    return render_template('6-number_odd_or_even.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
