ad1d9231d90cf49e233e7d0da17675f49726ef7d
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-07-31 18:45:44 +0100

    States List and html templates using Flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,5 +45,6 @@ def number_odd_or_even(n):
     '''even_number = n%2 == 0'''
     return render_template('6-number_odd_or_even.html', number=n)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
